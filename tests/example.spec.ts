1a152133d075554349a591996b17ff77bb7549b9
Lars Eckart <LarsEckart@users.noreply.github.com>
2022-01-26 13:27:01 +0200

    type a word/row

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,9 +15,13 @@ const words = readWords();
 test('basic test', async ({page}) => {
     await page.goto('https://www.powerlanguage.co.uk/wordle/');
     await page.click('game-modal path');
-    const letter = 's'
-    const buttonSelector = `button[data-key=${letter}]`;
-    await page.click(buttonSelector);
+    const guess = 'story'
+    const letters = guess.split('')
+
+    for (const letter of letters) {
+        const buttonSelector = `button[data-key=${letter}]`;
+        await page.click(buttonSelector);
+    }
 
     await page.pause()
 });
