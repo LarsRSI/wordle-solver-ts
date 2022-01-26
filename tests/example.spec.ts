53ea0a0565f4e9ca3899adb0324890305214c98d
Lars Eckart <LarsEckart@users.noreply.github.com>
2022-01-26 13:14:10 +0200

    type 1 letter

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,5 +15,9 @@ const words = readWords();
 test('basic test', async ({page}) => {
     await page.goto('https://www.powerlanguage.co.uk/wordle/');
     await page.click('game-modal path');
+    const letter = 's'
+    const buttonSelector = `button[data-key=${letter}]`;
+    await page.click(buttonSelector);
+
     await page.pause()
 });
