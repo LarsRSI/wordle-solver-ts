392e5eb307e4d3f84da9b65b066cb2140b37fa84
Lars Eckart <LarsEckart@users.noreply.github.com>
2022-01-26 13:40:24 +0200

    refactor, extract method

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test';
+import {Page, test} from '@playwright/test';
 import * as fs from 'fs';
 
 function readWords() {
@@ -12,18 +12,21 @@ function readWords() {
 const words = readWords();
 
 
-test('basic test', async ({page}) => {
-    await page.goto('https://www.powerlanguage.co.uk/wordle/');
-    await page.click('game-modal path');
-
-    const guess = 'story'
-    const letters = guess.split('')
+async function guess(page: Page, word: string) {
+    const letters = word.split('')
     for (const letter of letters) {
         const buttonSelector = `button[data-key=${letter}]`;
         await page.click(buttonSelector);
     }
     await page.click('text=enter');
     await new Promise(f => setTimeout(f, 1800));
-    
+}
+
+test('basic test', async ({page}) => {
+    await page.goto('https://www.powerlanguage.co.uk/wordle/');
+    await page.click('game-modal path');
+
+    await guess(page, 'story');
+
     await page.pause()
 });
