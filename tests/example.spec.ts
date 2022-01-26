96b65a9a97b6c0b2105be0af6c561cf226de4358
Lars Eckart <LarsEckart@users.noreply.github.com>
2022-01-26 13:06:13 +0200

    read file of 5 letter words

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,21 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
+import * as fs from 'fs';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await page.locator('text=Get started').click();
-  await expect(page).toHaveTitle(/Getting started/);
+function readWords() {
+    const words: string[] = [];
+    fs.readFileSync('words.txt', 'utf-8').split(/\r?\n/).forEach(function (line) {
+        words.push(line)
+    })
+    return words;
+}
+
+const words = readWords();
+
+
+test('basic test', async ({page}) => {
+    await page.goto('https://playwright.dev/');
+    await page.locator('text=Get started').click();
+
+    await page.pause()
+    await expect(page).toHaveTitle(/Getting started/);
 });
