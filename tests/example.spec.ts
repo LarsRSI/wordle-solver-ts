baacb97f8edaacdcd84ff5b0a9706b3c0333ced8
Lars Eckart <eckart@rushstreetinteractive.com>
2022-02-14 10:07:17 +0200

    make instructions disappear

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test('verify loss at end of playing', async ({ page }) => {
   await page.goto('https://www.powerlanguage.co.uk/wordle/');
   await page.click('id=pz-gdpr-btn-reject')
+  await page.click('game-modal svg');
   await page.pause();
 });
